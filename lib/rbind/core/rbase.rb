f16dc3feac434a372341687984f9b40cb6e4630b
Alexander Duda <Alexander.Duda@dfki.de>
2013-11-09 11:30:14 +0100

    fix copy paste error

--- lib/rbind/core/rbase.rb
+++ lib/rbind/core/rbase.rb
@@ -21,7 +21,7 @@ module Rbind
                 cn = "#{cprefix}#{name.gsub("::","_")}"
                 if cn =~ /operator/
                     cn = cn.gsub("operator++","operator_plusplus")
-                    cn = cn.gsub("operator++","operator_minusminus")
+                    cn = cn.gsub("operator--","operator_minusminus")
                     cn = cn.gsub("operator()","operator_fct")
                     cn = cn.gsub("operator!=","operator_unequal")
                     cn = cn.gsub("operator==","operator_equal")
